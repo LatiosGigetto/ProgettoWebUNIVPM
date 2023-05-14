7ccee5cd3a6aca52cfea1f1079a208bf7351f82a
amir-othmani <amir.othmani@hotmail.it>
2023-05-14 17:50:58 +0200

    More pages and headache

---
+++ resources/views/lista-aziende.blade.php
@@ -0,0 +1,67 @@
+<!DOCTYPE html>
+
+<html lang="it">
+
+@extends('header-footer')
+
+<head>
+    <meta charset="UTF-8">
+    @section("title")
+        Lista aziende
+    @endsection
+    <link rel="stylesheet" href="css/style.css">
+    <style>
+        .imgage_logo{
+            height: 100%;
+            float: left;
+            border: 1px solid red;
+        }
+        h3{
+            text-align: center;
+            border: 1px solid blue;
+            align-self: start;
+            float: left;
+            margin-top: 0;
+            width: 100%;
+            margin-bottom: 0;
+        }
+        p{
+            border: 1px solid violet;
+            align-self: start;
+            width: 100%;
+            margin-top:0;
+            height: 100%;
+            margin-bottom: 0;
+        }
+    </style>
+
+</head>
+<body>
+@section('content')
+    <div style="min-height: 50vh">
+        <div class="spazio_blocco">
+            <div class="blocco_offerta">
+                <img src="images/xampp_logo.png" alt="questo è letteralmente il logo di Xampp" class="imgage_logo">
+                <div class="spazio_contenuto">
+                    <h3>Azienda 1</h3>
+                    <p>Dati azienda</p>
+                </div>
+            </div>
+            <div class="blocco_offerta">
+                <img src="images/xampp_logo.png" alt="questo è letteralmente il logo di Xampp" class="imgage_logo">
+                <div class="spazio_contenuto">
+                    <h3>Azienda 2</h3>
+                    <p>Dati azienda</p>
+                </div>
+            </div>
+            <div class="blocco_offerta">
+                <img src="images/xampp_logo.png" alt="questo è letteralmente il logo di Xampp" class="imgage_logo">
+                <div class="spazio_contenuto">
+                    <h3>Azienda 3</h3>
+                    <p>Dati azienda</p>
+                </div>
+            </div>
+        </div>
+    </div>
+</body>
+@endsection
--- resources/views/registrazione.blade.php
+++ resources/views/registrazione.blade.php
@@ -10,63 +10,65 @@
         <meta charset="UTF-8">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
     </head>
-
+    <body>
     @section('content')
-    <div style="text-align: center; border: 4px solid blue; margin-top: 5%; margin-left: 25%; margin-right: 25%; margin-bottom: 5%">
-        <p>Inserisci i dati per la registrazione</p>
+        <div style="text-align: center; border: 4px solid blue; margin-top: 5%; margin-left: 25%; margin-right: 25%; margin-bottom: 5%">
+            <p>Inserisci i dati per la registrazione</p>
 
-        <div>
-            <label>Nome utente</label> 
+            <div>
+                <label>Nome utente</label>
 
-            <form name="username" style="margin: 2%">
-                <input required>
-            </form>
+                <form name="username" style="margin: 2%">
+                    <input required>
+                </form>
 
-            <label>Password</label> 
+                <label>Password</label>
 
-            <form name="password" style="margin: 2%">
-                <input required>
-            </form>    
-            
-            <label>Nome</label> 
-            
-            <form name="nome" style="margin: 2%">
-                <input required>
-            </form>   
-            
-            <label>Cognome</label> 
-            
-            <form name="cognome" style="margin: 2%">
-                <input required>
-            </form> 
-            
-            <label>Genere</label> 
-            
-            <select name="genere" style="margin: auto; margin-top: 2%; margin-bottom: 2%; display: block">
-                <option value="M">Maschio</option>
-                <option value="F">Femmina</option>
-            </select>  
-            
-            <label>Età</label> 
-            
-            <form name="eta" style="margin: 2%">
-                <input type="number" min="1">
-            </form>   
-            
-            <label>Indirizzo email</label> 
-            
-            <form name="email" style="margin: 2%">
-                <input type="email">
-            </form>   
-            
-            <button id="registerbutton" style="margin: 2%">
-                Registrati
-            </button>
+                <form name="password" style="margin: 2%">
+                    <input required>
+                </form>
 
+                <label>Nome</label>
+
+                <form name="nome" style="margin: 2%">
+                    <input required>
+                </form>
+
+                <label>Cognome</label>
+
+                <form name="cognome" style="margin: 2%">
+                    <input required>
+                </form>
+
+                <label>Genere</label>
+
+                <select name="genere" style="margin: auto; margin-top: 2%; margin-bottom: 2%; display: block">
+                    <option value="M">Maschio</option>
+                    <option value="F">Femmina</option>
+                </select>
+
+                <label>Età</label>
+
+                <form name="eta" style="margin: 2%">
+                    <input type="number" min="1">
+                </form>
+
+                <label>Indirizzo email</label>
+
+                <form name="email" style="margin: 2%">
+                    <input type="email">
+                </form>
+
+                <button id="registerbutton" style="margin: 2%">
+                    Registrati
+                </button>
+
+
+            </div>
 
         </div>
+    </body>
 
-    </div>
-    @endsection    
+    @endsection
 
 </html>
