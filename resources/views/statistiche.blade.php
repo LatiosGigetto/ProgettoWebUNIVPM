781602e0063417b52f489db94f4edee1b507c94c
leonardodipietro <S1101664@studentiunivpm.it>
2023-05-14 17:53:42 +0200

    statistiche

--- resources/views/statistiche.blade.php
+++ resources/views/statistiche.blade.php
@@ -12,11 +12,22 @@
     <link rel="stylesheet" href="css/style.css">
 
     <style>
+     .margine {
+         margin-top:50px;
+     }
+     table {
+         border-collapse: collapse;
+         width: 100%;
+     }
 
+     th, td {
+         border: 1px solid black;
+         padding: 8px;
+     }
 
-        .numerocoupon{
-            margin-top: 80px;
-        }
+     th {
+         background-color: #f2f2f2;
+     }
 
     </style>
 
@@ -24,36 +35,75 @@
 
 @section("content")
 
-    <body>
-
-
-
-    <div style="display: flex">
-        <div style="display: flex">
-            <div class="spazio_barra_laterale">
-                *spazio per la barra laterale*
-            </div>
-
-
-            <div class="spazio_blocco">
-
-
-
-
-
-            </div>
-
-            <h1>Statistiche</h1>
-
-
-
-        <div>
-            <div class="numerocoupon" name="numerocoupon" id="numerocoupon">
-                Numero totale coupo emessi:
-            </div>
-        </div>
-        </div>
+<body>
+<!--
+    <div class="spazio_barra_laterale">
+        *spazio per la barra laterale*
     </div>
+-->
+
+<h1>Statistiche</h1>
+<div class="margine"> <strong>numero totale coupone emessi: </strong></div>
+
+<div class="margine"><strong>lista coupon</strong></div>
+<div class="margine">
+    <table>
+        <tbody>
+        <tr>
+            <td>Promozione 1</td>
+            <td>
+                <button name="vediprom1" id="vediprom1">vedi numero coupon</button>
+            </td>
+            <td></td>
+        </tr>
+        <tr>
+            <td>Promozione 2</td>
+            <td>
+                <button name="vediprom2" id="vediprom2">vedi numero coupon</button>
+            </td>
+            <td></td>
+        </tr>
+        <tr>
+            <td>Promozione 3</td>
+            <td>
+                <button name="vediprom3" id="vediprom3">vedi numero coupon</button>
+            </td>
+         <td></td>
+        </tr>
+
+        </tbody>
+    </table>
+</div>
+
+<div class="margine"> <strong>lista clienti </strong></div>
+<div class="margine">
+    <table>
+        <tbody>
+        <tr>
+            <td>CLiente 1</td>
+            <td>
+                <button name="vedicli1" id="vedicli1">vedi numero coupon</button>
+            </td>
+            <td></td>
+        </tr>
+        <tr>
+            <td>Cliente 2</td>
+            <td>
+                <button name="vedicli2" id="vedicli2">vedi numero coupon</button>
+            </td>
+            <td></td>
+        </tr>
+        <tr>
+            <td>Cliente 3</td>
+            <td>
+                <button name="vedicli3" id="vedicli3">vedi numero coupon</button>
+            </td>
+           <td></td>
+        </tr>
+
+        </tbody>
+    </table>
+</div>
     </body>
 
 @endsection
