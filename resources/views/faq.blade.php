22c94e1cd626221c4ee9446a6407db4045cc0943
amir-othmani <amir.othmani@hotmail.it>
2023-05-15 15:29:44 +0200

    Episode 2

--- resources/views/faq.blade.php
+++ resources/views/faq.blade.php
@@ -7,6 +7,11 @@
         Le FAQ
     @endsection
     <link rel="stylesheet" href="css/style.css">
+    <style>
+        #risposta {
+            font-size: 13px;
+        }
+    </style>
 </head>
 <body>
 @section("content")
@@ -15,13 +20,13 @@
     <h1 style="text-align: center">Domande frequenti</h1>
 
     <h4>Q: Domanda 1</h4>
-    <p style="font-size: 13px">A: Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque vel viverra justo, vitae auctor nunc. Ut vitae eleifend eros, vel interdum leo. Pellentesque sit amet elementum leo. Donec sed lacus dui. In fringilla molestie est, eu lacinia enim feugiat in. Duis mattis tempus semper. Vestibulum bibendum ac ex id ultrices. Fusce metus quam, hendrerit eu fringilla ac, accumsan sit amet urna. Morbi eget tincidunt neque. Proin elementum nibh at euismod hendrerit. Vivamus malesuada ipsum turpis, accumsan imperdiet libero placerat non. Quisque sit amet tellus eu nunc faucibus maximus nec eget tellus. </p>
+    <p id="risposta">A: Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque vel viverra justo, vitae auctor nunc. Ut vitae eleifend eros, vel interdum leo. Pellentesque sit amet elementum leo. Donec sed lacus dui. In fringilla molestie est, eu lacinia enim feugiat in. Duis mattis tempus semper. Vestibulum bibendum ac ex id ultrices. Fusce metus quam, hendrerit eu fringilla ac, accumsan sit amet urna. Morbi eget tincidunt neque. Proin elementum nibh at euismod hendrerit. Vivamus malesuada ipsum turpis, accumsan imperdiet libero placerat non. Quisque sit amet tellus eu nunc faucibus maximus nec eget tellus. </p>
 
     <h4>Q: Domanda 2</h4>
-    <p style="font-size: 13px">A: Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque vel viverra justo, vitae auctor nunc. Ut vitae eleifend eros, vel interdum leo. Pellentesque sit amet elementum leo. Donec sed lacus dui. In fringilla molestie est, eu lacinia enim feugiat in. Duis mattis tempus semper. Vestibulum bibendum ac ex id ultrices. Fusce metus quam, hendrerit eu fringilla ac, accumsan sit amet urna. Morbi eget tincidunt neque. Proin elementum nibh at euismod hendrerit. Vivamus malesuada ipsum turpis, accumsan imperdiet libero placerat non. Quisque sit amet tellus eu nunc faucibus maximus nec eget tellus. </p>
+    <p id="risposta">A: Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque vel viverra justo, vitae auctor nunc. Ut vitae eleifend eros, vel interdum leo. Pellentesque sit amet elementum leo. Donec sed lacus dui. In fringilla molestie est, eu lacinia enim feugiat in. Duis mattis tempus semper. Vestibulum bibendum ac ex id ultrices. Fusce metus quam, hendrerit eu fringilla ac, accumsan sit amet urna. Morbi eget tincidunt neque. Proin elementum nibh at euismod hendrerit. Vivamus malesuada ipsum turpis, accumsan imperdiet libero placerat non. Quisque sit amet tellus eu nunc faucibus maximus nec eget tellus. </p>
 
     <h4>Q: Domanda 3</h4>
-    <p style="font-size: 13px">A: Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque vel viverra justo, vitae auctor nunc. Ut vitae eleifend eros, vel interdum leo. Pellentesque sit amet elementum leo. Donec sed lacus dui. In fringilla molestie est, eu lacinia enim feugiat in. Duis mattis tempus semper. Vestibulum bibendum ac ex id ultrices. Fusce metus quam, hendrerit eu fringilla ac, accumsan sit amet urna. Morbi eget tincidunt neque. Proin elementum nibh at euismod hendrerit. Vivamus malesuada ipsum turpis, accumsan imperdiet libero placerat non. Quisque sit amet tellus eu nunc faucibus maximus nec eget tellus. </p>
+    <p id="risposta">A: Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque vel viverra justo, vitae auctor nunc. Ut vitae eleifend eros, vel interdum leo. Pellentesque sit amet elementum leo. Donec sed lacus dui. In fringilla molestie est, eu lacinia enim feugiat in. Duis mattis tempus semper. Vestibulum bibendum ac ex id ultrices. Fusce metus quam, hendrerit eu fringilla ac, accumsan sit amet urna. Morbi eget tincidunt neque. Proin elementum nibh at euismod hendrerit. Vivamus malesuada ipsum turpis, accumsan imperdiet libero placerat non. Quisque sit amet tellus eu nunc faucibus maximus nec eget tellus. </p>
 </div>
 </body>
 @endsection
--- resources/views/lista-aziende.blade.php
+++ resources/views/lista-aziende.blade.php
@@ -10,54 +10,31 @@
         Lista aziende
     @endsection
     <link rel="stylesheet" href="css/style.css">
-    <style>
-        .imgage_logo{
-            height: 100%;
-            float: left;
-            border: 1px solid red;
-        }
-        h3{
-            text-align: center;
-            border: 1px solid blue;
-            align-self: start;
-            float: left;
-            margin-top: 0;
-            width: 100%;
-            margin-bottom: 0;
-        }
-        .paragraph{
-            border: 1px solid violet;
-            align-self: start;
-            width: 100%;
-            margin-top:0;
-            height: 100%;
-            margin-bottom: 0;
-        }
-    </style>
-
+    <link rel="stylesheet" href="css/aziende.css">
 </head>
+
 <body>
 @section('content')
     <div style="min-height: 50vh">
         <div class="spazio_blocco">
             <div class="blocco_offerta">
-                <img src="images/xampp_logo.png" alt="questo è letteralmente il logo di Xampp" class="imgage_logo">
+                <img src="images/xampp_logo.png" alt="questo è letteralmente il logo di Xampp" class="image_logo">
                 <div class="spazio_contenuto">
-                    <h3>Azienda 1</h3>
+                    <h3 class="titolo_azienda">Azienda 1</h3>
                     <p class="paragraph">Dati azienda</p>
                 </div>
             </div>
             <div class="blocco_offerta">
-                <img src="images/xampp_logo.png" alt="questo è letteralmente il logo di Xampp" class="imgage_logo">
+                <img src="images/xampp_logo.png" alt="questo è letteralmente il logo di Xampp" class="image_logo">
                 <div class="spazio_contenuto">
-                    <h3>Azienda 2</h3>
+                    <h3 class="titolo_azienda">Azienda 2</h3>
                     <p class="paragraph">Dati azienda</p>
                 </div>
             </div>
             <div class="blocco_offerta">
-                <img src="images/xampp_logo.png" alt="questo è letteralmente il logo di Xampp" class="imgage_logo">
+                <img src="images/xampp_logo.png" alt="questo è letteralmente il logo di Xampp" class="image_logo">
                 <div class="spazio_contenuto">
-                    <h3>Azienda 3</h3>
+                    <h3 class="titolo_azienda">Azienda 3</h3>
                     <p class="paragraph">Dati azienda</p>
                 </div>
             </div>
