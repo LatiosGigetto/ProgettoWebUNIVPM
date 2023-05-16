bdcad24edf06fb86327b89acc6cd5aaeea729f7a
Alessandro Rossini <latiosgigetto@gmail.com>
2023-05-16 11:32:38 +0200

    Added Model prototypes

---
+++ app/Models/Azienda.php
@@ -0,0 +1,15 @@
+<?php
+
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Scripting/PHPClass.php to edit this template
+ */
+
+/**
+ * Description of Azienda
+ *
+ * @author Oathbound
+ */
+class Azienda {
+    //put your code here
+}
---
+++ app/Models/Offerta.php
@@ -0,0 +1,15 @@
+<?php
+
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Scripting/PHPClass.php to edit this template
+ */
+
+/**
+ * Description of Offerta
+ *
+ * @author Oathbound
+ */
+class Offerta {
+    //put your code here
+}
