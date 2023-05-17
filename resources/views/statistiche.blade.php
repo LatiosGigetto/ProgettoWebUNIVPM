269e6536faf17d91b7d6d247964e6bfe907bfa92
Francesco Parisi <francescoparisi178@gmail.com>
2023-05-17 12:40:14 +0200

    aaaaaa

--- resources/views/statistiche.blade.php
+++ resources/views/statistiche.blade.php
@@ -26,69 +26,70 @@
     -->
 
     <h1>Statistiche</h1>
-    <div class="container" style="justify-content: start">
-        <div id="marginestats"> <strong>numero totale coupone emessi: </strong></div>
+        <div style="padding: 50px">
+            <div>
+                <strong>numero totale coupone emessi: </strong>
+            </div>
+            <div style="margin-top: 10px"></div>
+                <strong>lista coupon</strong>
+                <table>
+                    <tbody>
+                    <tr>
+                        <td>Promozione 1</td>
+                        <td>
+                            <button name="vediprom1" id="vediprom1">vedi numero coupon</button>
+                        </td>
 
-        <div class="margine"><strong>lista coupon</strong></div>
+                    </tr>
+                    <tr>
+                        <td>Promozione 2</td>
+                        <td>
+                            <button name="vediprom2" id="vediprom2">vedi numero coupon</button>
+                        </td>
 
-        <div class="margine">
-            <table>
-                <tbody>
-                <tr>
-                    <td>Promozione 1</td>
-                    <td>
-                        <button name="vediprom1" id="vediprom1">vedi numero coupon</button>
-                    </td>
-                    <td></td>
-                </tr>
-                <tr>
-                    <td>Promozione 2</td>
-                    <td>
-                        <button name="vediprom2" id="vediprom2">vedi numero coupon</button>
-                    </td>
-                    <td></td>
-                </tr>
-                <tr>
-                    <td>Promozione 3</td>
-                    <td>
-                        <button name="vediprom3" id="vediprom3">vedi numero coupon</button>
-                    </td>
-                    <td></td>
-                </tr>
+                    </tr>
+                    <tr>
+                        <td>Promozione 3</td>
+                        <td>
+                            <button name="vediprom3" id="vediprom3">vedi numero coupon</button>
+                        </td>
 
-                </tbody>
-            </table>
+                    </tr>
+                    </tbody>
+                </table>
+            </div>
         </div>
-
-        <div class="margine"> <strong>lista clienti </strong></div>
-        <div class="margine">
-            <table>
-                <tbody>
-                <tr>
-                    <td>CLiente 1</td>
-                    <td>
-                        <button name="vedicli1" id="vedicli1">vedi numero coupon</button>
-                    </td>
-                    <td></td>
-                </tr>
-                <tr>
-                    <td>Cliente 2</td>
-                    <td>
-                        <button name="vedicli2" id="vedicli2">vedi numero coupon</button>
-                    </td>
-                    <td></td>
-                </tr>
-                <tr>
-                    <td>Cliente 3</td>
-                    <td>
-                        <button name="vedicli3" id="vedicli3">vedi numero coupon</button>
-                    </td>
-                    <td></td>
-                </tr>
-
-                </tbody>
-            </table>
+    <div style="padding: 50px">
+        <div>
+            <strong>numero totale coupone emessi: </strong>
         </div>
+        <div style="margin-top: 10px"></div>
+        <strong>lista coupon</strong>
+        <table>
+            <tbody>
+            <tr>
+                <td>Cliente 1</td>
+                <td>
+                    <button name="vediprom1" id="vediprom1">vedi numero coupon</button>
+                </td>
+
+            </tr>
+            <tr>
+                <td>Cliente 2</td>
+                <td>
+                    <button name="vediprom2" id="vediprom2">vedi numero coupon</button>
+                </td>
+
+            </tr>
+            <tr>
+                <td>CLiente 3</td>
+                <td>
+                    <button name="vediprom3" id="vediprom3">vedi numero coupon</button>
+                </td>
+            </tr>
+            </tbody>
+        </table>
+    </div>
     </div>
     </body>
 
