71316e0f51e134a6d995bc40d6a1f3525c0d84e5
Alessandro Rossini <latiosgigetto@gmail.com>
2023-05-17 18:06:29 +0200

    I have 3 total neurons

--- app/Http/Controllers/PublicController.php
+++ app/Http/Controllers/PublicController.php
@@ -1,32 +1,35 @@
 <?php
 
-
 namespace App\Http\Controllers;
 
 use App\Models\Azienda;
 use App\Models\Offerta;
 
-
 class PublicController extends Controller {
-    
-    // TODO: implementare per bene le viste da chiamare
-    
+
+    protected $_offertaModel;
+
+    public function __construct() {
+        $this->_offertaModel = new Offerta;
+    }
+
     // Funzione per prendere e paginare la lista offerte
-    
+
     public function showAziendeList() {
-        
+
         $aziende = Azienda::paginate(10);
-        
+
         return view('testPaginazione')->with('aziende', $aziende);
     }
-    
-    // Funzione per prendere e paginare la lista offerte
-    
-    public function showOfferteByAzienda($azienda) {
-        
-        $offerte = Offerta::paginate(10);
-        
+
+    // Funzione per prendere e paginare la lista offerte dato il nome di un azienda
+
+    public function showOfferteByAzienda($nomeAzienda) {
+
+        $offerte = $this->_offertaModel
+                        ->getOfferteByAzienda($nomeAzienda)::paginate(10);
+
         return view('testPaginazione')->with('offerte', $offerte);
     }
-    
+
 }
--- app/Models/Azienda.php
+++ app/Models/Azienda.php
@@ -11,4 +11,6 @@ class Azienda extends Model {
     public $timestamps = false;
 
     // TODO: inserire metodi utili
+
+
 }
