c5a38114618b97e6f6436aa8c242c68a754fe2f4
Alessandro Rossini <latiosgigetto@gmail.com>
2023-05-23 13:06:47 +0200

    Added ModificaInfo validation rules and disables username change temporarily

--- app/Http/Controllers/Auth/ModificainfoController.php
+++ app/Http/Controllers/Auth/ModificainfoController.php
@@ -6,43 +6,52 @@ use Illuminate\Support\Facades\Auth;
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 
-
-
-
-class ModificainfoController extends Controller
-{
+class ModificainfoController extends Controller {
     /*
-    public function __construct()
-    {
-        $this->middleware('auth');
-    }
-    */
+      public function __construct()
+      {
+      $this->middleware('auth');
+      }
+     */
 
-    public function  create(){
+    public function create() {
         return view('auth.modifica-info');
     }
 
-    public function store(Request $request)
-    {
-        //TODO missing validate
+    public function store(Request $request) {
+        $request->validate([
+            'nome' => ['nullable', 'string', 'max:30'],
+            'cognome' => ['nullable', 'string', 'max:30'],
+            'email' => ['nullable', 'string', 'email', 'max:30'],
+            //'username' => ['string', 'unique:utente', 'max:30'],
+            'telefono' => ['nullable', 'string', 'max:10'],
+            'età' => ['nullable', 'integer', 'max:999']
+        ]);
 
         $user = Auth::user();
         // Modifica delle informazioni dell'utente
-        //TODO cosi if da sistemare
-        if($request->input('nome')!= null)
-           $user->Nome = $request->input('nome');
-        if($request->input('cognome')!= null)
-           $user->Cognome = $request->input('cognome');
-        if($request->input('mail')!= null)
-           $user->Mail = $request->input('mail');
-        if($request->input('username')!= null)
-           $user->username = $request->input('username');
-        if($request->input('telefono')!= null)
-           $user->Telefono = $request->input('telefono');
-        if($request->input('età')!= null)
-           $user->Età = $request->input('età');
-        if($request->input('genere')!= null)
-           $user->Genere = $request->input('genere');
+
+        if ($request->input('nome') != null) {
+            $user->Nome = $request->input('nome');
+        }
+        if ($request->input('cognome') != null) {
+            $user->Cognome = $request->input('cognome');
+        }
+        if ($request->input('mail') != null) {
+            $user->Mail = $request->input('mail');
+        }
+       /* if ($request->input('username') != null) {
+            $user->username = $request->input('username');
+        } */
+        if ($request->input('telefono') != null) {
+            $user->Telefono = $request->input('telefono');
+        }
+        if ($request->input('età') != null) {
+            $user->Età = $request->input('età');
+        }
+        if ($request->input('genere') != null) {
+            $user->Genere = $request->input('genere');
+        }
         $user->save();
 
         return redirect()->back()->with('success', 'Informazioni modificate con successo!');
