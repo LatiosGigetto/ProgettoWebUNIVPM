ef5b32bd35caf4aa236c74709c4c1a64917f0714
amir-othmani <amir.othmani@hotmail.it>
2023-05-23 17:24:32 +0200

    I am a patacca

--- resources/views/dettagli-offerta.blade.php
+++ resources/views/dettagli-offerta.blade.php
@@ -10,31 +10,34 @@
                 <!--TODO: sarebbe carino dargli la stessa grafica delle cards del catologo-->
                 <h2>Dettagli</h2>
                 <img id = "logosito" src="{{asset("images/logosito.png")}}" alt="Descrizione dell'immagine">
-                
+
                 <a href="linkdaseguire">Nome azienda</a>
                 <br>
 
                 <strong>Descrizione offerta</strong>
-                
-                <p> {{ $offerta->Descrizione}} </p>
-                
+
+                <p> {{ $offerta->Descrizione }} </p>
+
                 <br>
-                
+
                 <strong>Luogo di fruizione</strong>
-                
-                <p> {{ $offerta->Luogo}}</p>
-                
+
+                <p>{{ $offerta->Luogo }}</p>
+
                 <div>
-                <button name="generaofferta" id="generaofferta" >
-                    genera
-                </button>
+                <a href="{{route('acquisto', ['id' => $offerta-> Id_Offerta])}}">
+                    <button name="generaofferta" id="generaofferta">
+                        genera
+                    </button>
+                </a>
+
                 <button name="indietro" id="indietro">
                     torna indietro
                 </button>
                 </div>
 
 
-    @endsection 
+    @endsection
 
 <!-- TODO FUNZIONE JAVASCRIPT DA CORREEGGERE PER METTERE VALORI DEL DATABASE
                 <script>
