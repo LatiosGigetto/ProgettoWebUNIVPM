0f3ca8af6f096c633b858cfaed5c4f7eacef17ed
leonardodipietro <S1101664@studentiunivpm.it>
2023-05-28 16:39:34 +0200

    css

--- resources/views/layouts/contenitore.blade.php
+++ resources/views/layouts/contenitore.blade.php
@@ -1,12 +1,16 @@
-@extends("layouts.header-footer")
+@extends('layouts.header-footer')
 
-<!-- Questo template funziona in maniera identica ad header-footer ma aggiunge   -->
-<!-- un riquadro centrale con caratteristiche di stile predefinite da riempire   -->
-<!-- con il contenuto definito nella pagina estesa nella section "contenuto".    -->
-
-
-@section("content")
-    <div id="contenitore">
-        @yield("contenuto")
+@section('content')
+    <div id="contenitore" class="container mt-3">
+        <div class="row justify-content-center">
+            <div class="col-md-8 col-lg-12 mb-4" style="background-image: url('{{asset('images/doge-to-the-moon-05.jpg')}}'); background-size: cover; background-position: center top;">
+                <div class="card" style="background-color: transparent;">
+                    <div class="card-body" style="background-color: rgba(255, 255, 255, 0.7);">
+                        @yield('contenuto')
+                    </div>
+                </div>
+            </div>
+        </div>
     </div>
 @endsection
+
--- resources/views/sezione-pubblica/contatti.blade.php
+++ resources/views/sezione-pubblica/contatti.blade.php
@@ -1,17 +1,16 @@
-<!--da rivedere: va centrato il blocco di testo-->
 @extends('layouts.contenitore')
 
-@section("title")
-Contatti
+@section('title')
+    Contatti
 @endsection
 
-
-@section("contenuto")
-<h1 id="titolo_sezione">Contatti</h1>
-<h3> Informazioni utili per contattare l'amministratore</h3>
-<address id="contatti">
-    <p>Amministratore: {{ $admin->Nome }} {{ $admin->Cognome}}</p>
-    <p> Email: {{ $admin->Mail  }}</p>
-    <p>Telefono: {{ $admin->Telefono }}</p>
-</address>
+@section('contenuto')
+    <h1 id="titolo_sezione">Contatti</h1>
+    <h3>Informazioni utili per contattare l'amministratore</h3>
+    <address id="contatti">
+        <p>Amministratore: {{ $admin->Nome }} {{ $admin->Cognome}}</p>
+        <p>Email: {{ $admin->Mail }}</p>
+        <p>Telefono: {{ $admin->Telefono }}</p>
+    </address>
 @endsection
+
