4490e2316042167864cb1f276349a29230dcc99b
amir-othmani <amir.othmani@hotmail.it>
2023-06-02 13:28:49 +0200

    Form WiP

--- resources/views/sezione-staff/gestione-promozioni.blade.php
+++ resources/views/sezione-staff/gestione-promozioni.blade.php
@@ -6,26 +6,20 @@
 @endsection
 
 @section("link-scripts")
-
     <link rel="stylesheet" href="{{asset("css/tabelle.css")}}">
-
     <script src="{{asset("js/gestione-promo.js")}}"></script>
-
 @endsection
 
-
 @section('content')
-
-    <div class="spazio_blocco">
+    <div class="container d-flex align-items-center h-100">
 
         @switch($azione)
 
-
             @case('view')
-                <div class="container ">
+                <div class="container">
                     <h1 class="text-center">Gestione Promozioni</h1>
 
-                    <table class=" table mx-auto ">
+                    <table class="table mx-auto">
                         <thead class="thead-dark">
                         <tr>
                             <th>ID Offerta</th>
@@ -74,137 +68,180 @@
 
                         @include('paginator.paginator', ['paginator' => $offerte])
                     </div>
-                    @break
-
-
-                    @case('mod')
-
-                        <h1 class="text-center">Modifica Offerta</h1>
-
-                        <div id="mod-off-sezione">
-
-                            {{ Form::open(['route' => 'modifica-offerta']) }}
-
-                            {{ Form::hidden('idOfferta', $offertaSel->Id_Offerta) }}
-
-                            <div class="form-group">
-                                {{ Form::label('oggetto', 'Oggetto') }}
-                                {{ Form::text('oggetto', $offertaSel->Oggetto, ['class' => 'form-control']) }}
-                                <br>
-                                @error('oggetto')
-                                <span style="color: red">{{ $message }}</span>
-                                @enderror
-                            </div>
-
-                            <div class="form-group">
-                                {{ Form::label('descrizione', 'Descrizione') }}
-                                {{ Form::textarea('descrizione', $offertaSel->Descrizione, ['class' => 'form-control']) }}
-                                @error('descrizione')
-                                <span style="color: red">{{ $message }}</span>
-                                @enderror
-                            </div>
-
-                            <div class="form-group">
-                                {{ Form::label('azienda', 'Azienda') }}
-                                {{ Form::select('azienda', $listaAziende, $offertaSel->Id_Azienda, ['class' => 'form-control']) }}
-                                @error('azienda')
-                                <span style="color: red">{{ $message }}</span>
-                                @enderror
-                            </div>
-
-                            <div class="form-group">
-                                {{ Form::label('luogo', 'Luogo') }}
-                                {{ Form::text('luogo', $offertaSel->Luogo, ['class' => 'form-control']) }}
-                                @error('luogo')
-                                <span style="color: red">{{ $message }}</span>
-                                @enderror
-                            </div>
-
-                            <div class="form-group">
-                                {{ Form::label('validità', 'Validità') }}
-                                {{ Form::text('validità', $offertaSel->Validità, ['class' => 'form-control']) }}
-                                @error('validità')
-                                <span style="color: red">{{ $message }}</span>
-                                @enderror
-                            </div>
-
-                            {{ Form::submit('Modifica', ['class' => 'btn btn-primary']) }}
-                            {{ Form::close() }}
-                        </div>
                 </div>
-                <a href="{{ route('gestione-promozioni') }}" class="btn btn-primary">Torna indietro</a>
+            @break
 
-    </div>
+            @case('mod')
+                <!--TODO: fixare la larghezza-->
+                <div class="container w-75 form_container my-2">
+                    <h1 class="text-center">Modifica Offerta</h1>
 
-    @break
+                    <div id="mod-off-sezione">
+                        {{ Form::open(['route' => 'modifica-offerta']) }}
+                        {{ Form::hidden('idOfferta', $offertaSel->Id_Offerta) }}
 
-    @case('create')
+                        <div class="form-group my-2">
+                            {{ Form::label('oggetto', 'Oggetto') }}
+                            {{ Form::text('oggetto', $offertaSel->Oggetto, ['class' => 'form-control']) }}
+                            @error('oggetto')
+                            <span style="color: red">{{ $message }}</span>
+                            @enderror
+                        </div>
 
-        <h1>Crea Offerta</h1>
+<!-- Il codice che segue è demoniaco -->
+{{--                        <div class="form-group">--}}
+{{--                            {{ Form::label('oggetto', 'Oggetto') }}--}}
+{{--                            {{ Form::text('oggetto', $offertaSel->Oggetto, ['class' => 'form-control']) }}--}}
+{{--                            <br>--}}
+{{--                            @error('oggetto')--}}
+{{--                            <span style="color: red">{{ $message }}</span>--}}
+{{--                            @enderror--}}
+{{--                        </div>--}}
 
-        <div id="crea-off-sezione">
+                        <div class="form-group my-2">
+                            {{ Form::label('descrizione', 'Descrizione') }}
+                            {{ Form::textarea('descrizione', $offertaSel->Descrizione, ['class' => 'form-control']) }}
+                            @error('descrizione')
+                            <span style="color: red">{{ $message }}</span>
+                            @enderror
+                        </div>
 
-            {{ Form::open(['route' => 'crea-offerta']) }}
+                        <div class="form-group my-2">
+                            {{ Form::label('azienda', 'Azienda') }}
+                            {{ Form::select('azienda', $listaAziende, $offertaSel->Id_Azienda, ['class' => 'form-control']) }}
+                            @error('azienda')
+                            <span style="color: red">{{ $message }}</span>
+                            @enderror
+                        </div>
 
-            <div style="margin: 2%">
-                {{ Form::label('oggetto', 'Oggetto') }}
-                {{ Form::text('oggetto', '') }}
-                <br>
-                @error('oggetto')
-                <span style="color: red">{{ $message }}</span>
-                @enderror
-            </div>
+                        <div class="form-group my-2">
+                            {{ Form::label('luogo', 'Luogo') }}
+                            {{ Form::text('luogo', $offertaSel->Luogo, ['class' => 'form-control']) }}
+                            @error('luogo')
+                            <span style="color: red">{{ $message }}</span>
+                            @enderror
+                        </div>
 
-            <div style="margin: 2%">
-                {{ Form::label('descrizione', 'Descrizione') }}
-                {{ Form::textarea('descrizione', '') }}
-                <br>
-                @error('descrizione')
-                <span style="color: red">{{ $message }}</span>
-                @enderror
-            </div>
+                        <div class="form-group my-2">
+                            {{ Form::label('validità', 'Validità') }}
+                            {{ Form::text('validità', $offertaSel->Validità, ['class' => 'form-control']) }}
+                            @error('validità')
+                            <span style="color: red">{{ $message }}</span>
+                            @enderror
+                        </div>
+                    </div>
+                    <div class="mt-4 mb-2">
+                        {{ Form::submit('Modifica', ['class' => 'btn btn-primary']) }}
+                        {{ Form::close() }}
+                        <a href="{{ route('gestione-promozioni') }}" class="btn btn-primary">Torna indietro</a>
+                    </div>
+                </div>
+            @break
 
-            <div style="margin: 2%">
-                {{ Form::label('azienda', 'Azienda') }}
-                {{ Form::select('azienda', $listaAziende) }}
-                <br>
-                @error('azienda')
-                <span style="color: red">{{ $message }}</span>
-                @enderror
-            </div>
+            @case('create')
+                <div class="container w-75 form_container my-2">
+                    <h1>Crea Offerta</h1>
 
+                    <div id="crea-off-sezione">
+                        {{ Form::open(['route' => 'crea-offerta']) }}
 
-            <div style="margin: 2%">
-                {{ Form::label('luogo', 'Luogo') }}
-                {{ Form::text('luogo', '' ) }}
-                <br>
-                @error('luogo')
-                <span style="color: red">{{ $message }}</span>
-                @enderror
-            </div>
+                        <div class="form-group my-2">
+                            {{ Form::label('oggetto', 'Oggetto') }}
+                            {{ Form::text('oggetto', '', ['class' => 'form-control']) }}
+                            @error('oggetto')
+                            <span style="color: red">{{ $message }}</span>
+                            @enderror
+                        </div>
+<!--TODO: Qualcuno mi spieghi perché questi blocchi di codice hanno un'altezza extra a caso-->
+{{--                        <div class="form-group my-2">--}}
+{{--                            {{ Form::label('oggetto', 'Oggetto') }}--}}
+{{--                            {{ Form::text('oggetto', '', ['class' => 'form-control']) }}--}}
+{{--                            <br>--}}
+{{--                            @error('oggetto')--}}
+{{--                            <span style="color: red">{{ $message }}</span>--}}
+{{--                            @enderror--}}
+{{--                        </div>--}}
 
-            <div style="margin: 2%">
-                {{ Form::label('validità', 'Validità') }}
-                {{ Form::text('validità', '') }}
-                <br>
-                @error('validità')
-                <span style="color: red">{{ $message }}</span>
-                @enderror
-            </div>
+                        <div class="form-group my-2">
+                            {{ Form::label('descrizione', 'Descrizione') }}
+                            {{ Form::textarea('descrizione', '', ['class' => 'form-control']) }}
+                            @error('descrizione')
+                            <span style="color: red">{{ $message }}</span>
+                            @enderror
+                        </div>
 
-            {{ Form::submit('Crea') }}
-            {{ Form::close() }}
+{{--                        <div class="form-group my-2">--}}
+{{--                            {{ Form::label('descrizione', 'Descrizione') }}--}}
+{{--                            {{ Form::textarea('descrizione', '', ['class' => 'form-control']) }}--}}
+{{--                            <br>--}}
+{{--                            @error('descrizione')--}}
+{{--                            <span style="color: red">{{ $message }}</span>--}}
+{{--                            @enderror--}}
+{{--                        </div>--}}
 
-            <a href="{{ route('gestione-promozioni') }}" class="btn btn-primary">Torna indietro</a>
+                        <div class="form-group my-2">
+                            {{ Form::label('azienda', 'Azienda') }}
+                            {{ Form::select('azienda', $listaAziende, '', ['class' => 'form-control']) }}
+                            @error('azienda')
+                            <span style="color: red">{{ $message }}</span>
+                            @enderror
+                        </div>
 
-        </div>
+{{--                        <div class="form-group my-2">--}}
+{{--                            {{ Form::label('azienda', 'Azienda') }}--}}
+{{--                            {{ Form::select('azienda', $listaAziende, '', ['class' => 'form-select']) }}--}}
+{{--                            <br>--}}
+{{--                            @error('azienda')--}}
+{{--                            <span style="color: red">{{ $message }}</span>--}}
+{{--                            @enderror--}}
+{{--                        </div>--}}
 
-        @break
+                        <div class="form-group my-2">
+                            {{ Form::label('luogo', 'Luogo') }}
+                            {{ Form::text('luogo', '', ['class' => 'form-control']) }}
+                            @error('luogo')
+                            <span style="color: red">{{ $message }}</span>
+                            @enderror
+                        </div>
+
+{{--                        <div class="form-group my-2">--}}
+{{--                            {{ Form::label('luogo', 'Luogo') }}--}}
+{{--                            {{ Form::text('luogo', '', ['class' => 'form-control']) }}--}}
+{{--                            <br>--}}
+{{--                            @error('luogo')--}}
+{{--                            <span style="color: red">{{ $message }}</span>--}}
+{{--                            @enderror--}}
+{{--                        </div>--}}
+
+                        <div class="form-group my-2">
+                            {{ Form::label('validità', 'Validità') }}
+                            {{ Form::text('validità', '', ['class' => 'form-control']) }}
+                            @error('validità')
+                            <span style="color: red">{{ $message }}</span>
+                            @enderror
+                        </div>
+
+{{--                        <div class="form-group my-2">--}}
+{{--                            {{ Form::label('validità', 'Validità') }}--}}
+{{--                            {{ Form::text('validità', '', ['class' => 'form-control']) }}--}}
+{{--                            <br>--}}
+{{--                            @error('validità')--}}
+{{--                            <span style="color: red">{{ $message }}</span>--}}
+{{--                            @enderror--}}
+{{--                        </div>--}}
+
+                        <div class="mt-4 mb-2">
+                            {{ Form::submit('Crea', ['class' => 'btn btn-primary']) }}
+                            {{ Form::close() }}
+
+                            <a href="{{ route('gestione-promozioni') }}" class="btn btn-primary">Torna indietro</a>
+                        </div>
+                    </div>
+                </div>
+            @break
 
         @endswitch
 
-
-        </div>
-
-        @endsection
+    </div>
+@endsection
 
